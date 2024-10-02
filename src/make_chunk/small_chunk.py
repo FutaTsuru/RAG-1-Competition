2a013930f52a943ed643170eb093e88856f32d2f
FutaTsuru <tsuruharafuta@icloud.com>
2024-10-02 12:00:23 +0900

    improve system prompt

--- src/make_chunk/small_chunk.py
+++ src/make_chunk/small_chunk.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from collections import defaultdict
 from langchain.text_splitter import CharacterTextSplitter
 
 from config import setting
@@ -10,13 +11,18 @@ def make_and_save_small_chunk(documents, chunk_size: int, chunk_overlap: int, se
     text_splitter = CharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap, separator=separator)
     texts = text_splitter.split_documents(documents)
 
+    title_counter = defaultdict(int)
     splited_texts = []
     title_list = []
 
     for text in texts:
         title = text.metadata['title']
         title_list.append(title)
-        splited_text = f"{title}: {text.page_content}"
+
+        title_counter[title] += 1
+        n = title_counter[title]
+
+        splited_text = f"{title}の{n}番目の文章: {text.page_content}"
         splited_texts.append(splited_text)
     
     chunk_dict = {"title": title_list, "chunk": splited_texts}
