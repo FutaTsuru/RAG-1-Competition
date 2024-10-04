f56495d414be94c2c034dd106c9c771d7085ff62
ShinKengo <142248616+ShinKengo@users.noreply.github.com>
2024-10-05 00:33:09 +0900

    回答例も生成するように変更

--- src/analyze_query/query_analysis.py
+++ src/analyze_query/query_analysis.py
@@ -42,37 +42,41 @@ def extract_words(text):
     keyword_pattern = r"キーワード：(.+)"
     related_word_pattern = r"関連ワード：(.+)"
     important_word_pattern = r"最重要ワード：(.+)"
+    answer_example_pattern = r"回答例：(.+)"
     
     # キーワードの部分を取得
     keyword_match = re.search(keyword_pattern, text)
     related_word_match = re.search(related_word_pattern, text)
     important_word_match = re.search(important_word_pattern, text)
-    if not(keyword_match) or not(related_word_match) or not(important_word_match):
-        return "","",""
+    answer_example_match = re.search(answer_example_pattern, text)
+    if not(keyword_match) or not(related_word_match) or not(important_word_match) or not(answer_example_match):
+        return "","","",""
     keyword = keyword_match.group(1).strip().replace(","," ").replace("　"," ")
     related_word = related_word_match.group(1).strip().replace(","," ").replace("　"," ")
     important_word = important_word_match.group(1).strip().replace(","," ").replace("　"," ")
-    return keyword, related_word, important_word
+    answer_example = answer_example_match.group(1).strip().replace(","," ").replace("　"," ")
+    return keyword, related_word, important_word, answer_example
 
 def main():
-    data = {"index": [], "keyword": [], "related_word": [], "important_word": []}
+    data = {"index": [], "keyword": [], "related_word": [], "important_word": [], "answer_example": []}
     question_db = pd.read_csv("./question/query.csv")
     system_prompt = load_system_prompt(setting.ANALYZE_SYSTEM_PROMPT_PATH)
     for _, row in tqdm(question_db.iterrows(), total=len(question_db), desc="回答生成"):
         index = row['index']
         query = row['problem']
-        keyword, related_word, important_word = "","",""
+        keyword, related_word, important_word, answer_example = "","","",""
         s = 0
-        while keyword == "" or related_word == "" or important_word == "":
+        while keyword == "" or related_word == "" or important_word == "" or answer_example == "":
             response = ask_gpt4(query, system_prompt)
-            keyword, related_word, important_word = extract_words(response)
-            index += 1
-            if s > 0:
-                tqdm.write(f"質問{index}の試行回数: {s}")
+            keyword, related_word, important_word, answer_example = extract_words(response)
+            s += 1
+            if s > 1:
+                tqdm.write(f"質問{index}の試行回数: {s-1}")
         data["index"].append(index)
         data["keyword"].append(keyword)
         data["related_word"].append(related_word)
         data["important_word"].append(important_word)
+        data["answer_example"].append(answer_example)
     keyword_db = pd.DataFrame(data)
     keyword_db.to_csv(setting.KEYWORD_PATH, index=False, header=False)
 
